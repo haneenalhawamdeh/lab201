49cfb50821ad576e58ba31bd4b2d3696dbf0b539
haneen <your email here in single quotes>
2019-10-07 22:27:45 +0300

    add

---
+++ lab2/ordar2.js
@@ -0,0 +1,82 @@
+'use strict';
+var user = prompt("hello , what is your name dud ?");
+// console.log('hi'+user+'we are happy to see you here' )
+alert('hi'+user+'we are happy to see you here')
+var name = prompt("do you guess what is my name");
+name = name.toLowerCase();
+
+if (name == 'yes')
+{
+    alert('you are clever');
+} else if (name == 'y'){
+    alert('you are clever');
+} else if (name == 'no') {
+    alert('try again , trial for free for you dud');
+} else if (name == 'n') {
+    alert('try again , trial for free for you dud');
+}
+else 
+alert('please answer with yes or no')
+// console.log(try again , trial for free for you dud);
+
+var old = prompt("have you guessed what the lucky year that i was born in it  ");
+old=  old.toLocaleLowerCase();
+if (  old == 'yes') {
+    alert('i think it is done just by luck ');
+} else if ( old == 'y')
+   { alert('i think it is done just by luck ');
+} else if  (old == 'no') {
+    alert('you broken my heart');
+} else if (  old == 'n') {
+    alert('you brokn my heart');
+}
+else
+ alert ('please answer with yes or no')
+//  console.log('you broken my heart);
+ 
+var uni = prompt(" do you think that you know where did i studed?");
+uni= uni.toUpperCase();
+if ( uni == 'yes') {
+    alert(' that is not bad , you know good info about me i think ');
+}else if ( uni== 'y')
+   { alert('that is not bad , you know good info about me i think');
+} else if  (uni == 'no') {
+    alert('keep in mind , its good to have agood info about afamous one like me ');
+} else if ( uni == 'n') {
+    alert('keep in mind , its good to have agood info about afamous one like me');
+}
+else
+ alert ('please answer with yes or no')
+//  console.log('that is not bad , you know good info about me i think');
+
+ var love = prompt(" do you know what i fall in love with , i wll be surprised if you say yes  ?");
+love=  love.toUpperCase();
+if ( love == 'yes') {
+    alert('am happy to hear that ');
+} else if ( love == 'y')
+   { alert('am happy to hear that');
+} else if  (love == 'no') {
+    alert('that is abad news for me');
+} else if ( love == 'n') {
+    alert('that is abad news for me ');
+}
+else
+ alert ('please answer with yes or no')
+//  console.log('that is abad news for me');
+
+ var where = prompt(" do you know where am from   ?");
+where=  where.toUpperCase();
+if ( where == 'yes') {
+    alert('yaaaa that is good  ');
+} else if ( where == 'y')
+   { alert('yaaaa that is good ');
+} else if  (where == 'no') {
+    alert(' bad bad bad ');
+} else if ( where == 'n') {
+    alert('bad bad bad  ');
+}
+else
+ alert ('please answer with yes or no')
+//  console.log('bad bad bad');
+
+
