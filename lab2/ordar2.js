536d5d4ba6eda869b81b48fa6fee9cb3967dae77
haneen <your email here in single quotes>
2019-10-09 01:29:41 +0300

    dd

--- lab2/ordar2.js
+++ lab2/ordar2.js
@@ -1,82 +1,107 @@
-'use strict';
+    'use strict';
 var user = prompt("hello , what is your name dud ?");
 // console.log('hi'+user+'we are happy to see you here' )
-alert('hi'+user+'we are happy to see you here')
-var name = prompt("do you guess what is my name");
-name = name.toLowerCase();
+alert('hi  ' + user + ' we are happy to see you here')
+var counter = 0;
+var qu = [
+    ['do you guess what is my name?',
+        'you are clever',
+        'try again , trial for free for you dud'
+    ],
+    ['have you guessed what the lucky year that i was born in it',
+        'i think it is done just by luck ',
+        'you brokn my heart'
 
-if (name == 'yes')
-{
-    alert('you are clever');
-} else if (name == 'y'){
-    alert('you are clever');
-} else if (name == 'no') {
-    alert('try again , trial for free for you dud');
-} else if (name == 'n') {
-    alert('try again , trial for free for you dud');
-}
-else 
-alert('please answer with yes or no')
-// console.log(try again , trial for free for you dud);
+    ],
+    ['do you think that you know where did i studed?',
+        ' that is not bad , you know good info about me i think ',
+        'keep in mind , its good to have agood info about afamous one like me '
 
-var old = prompt("have you guessed what the lucky year that i was born in it  ");
-old=  old.toLocaleLowerCase();
-if (  old == 'yes') {
-    alert('i think it is done just by luck ');
-} else if ( old == 'y')
-   { alert('i think it is done just by luck ');
-} else if  (old == 'no') {
-    alert('you broken my heart');
-} else if (  old == 'n') {
-    alert('you brokn my heart');
-}
-else
- alert ('please answer with yes or no')
-//  console.log('you broken my heart);
- 
-var uni = prompt(" do you think that you know where did i studed?");
-uni= uni.toUpperCase();
-if ( uni == 'yes') {
-    alert(' that is not bad , you know good info about me i think ');
-}else if ( uni== 'y')
-   { alert('that is not bad , you know good info about me i think');
-} else if  (uni == 'no') {
-    alert('keep in mind , its good to have agood info about afamous one like me ');
-} else if ( uni == 'n') {
-    alert('keep in mind , its good to have agood info about afamous one like me');
-}
-else
- alert ('please answer with yes or no')
-//  console.log('that is not bad , you know good info about me i think');
+    ],
+    ['do you know what i fall in love with , i wll be surprised if you say yes',
+        'am happy to hear that',
+        'that is abad news for me'
 
- var love = prompt(" do you know what i fall in love with , i wll be surprised if you say yes  ?");
-love=  love.toUpperCase();
-if ( love == 'yes') {
-    alert('am happy to hear that ');
-} else if ( love == 'y')
-   { alert('am happy to hear that');
-} else if  (love == 'no') {
-    alert('that is abad news for me');
-} else if ( love == 'n') {
-    alert('that is abad news for me ');
-}
-else
- alert ('please answer with yes or no')
-//  console.log('that is abad news for me');
+    ],
+    ['do you know where am from',
+        'yaaaa that is good',
+        ' bad bad bad '
 
- var where = prompt(" do you know where am from   ?");
-where=  where.toUpperCase();
-if ( where == 'yes') {
-    alert('yaaaa that is good  ');
-} else if ( where == 'y')
-   { alert('yaaaa that is good ');
-} else if  (where == 'no') {
-    alert(' bad bad bad ');
-} else if ( where == 'n') {
-    alert('bad bad bad  ');
+    ],
+
+
+];
+for (var i = 0;i< qu.length;i++) {
+    var currentq = qu[i];
+// here we have array for 3 items which is prompt ,+ response,-response
+var promptindex = 0;
+var posindex = 1;
+var negindex = 2;
+var currentprompt = currentq[promptindex];
+var userresponse = prompt(currentprompt);
+userresponse = userresponse.toLowerCase();
+var currentposi = currentq[posindex];
+var currentneg = currentq[negindex];
+switch (userresponse) {
+    case 'yes':
+    case 'y':
+        counter = counter+1;
+      
+        alert(currentposi);
+        break;
+    case 'no':
+    case 'n':
+           
+        alert(currentneg);
+        break;
+        default:
+            alert ('wrong answer');}
+            
 }
-else
- alert ('please answer with yes or no')
-//  console.log('bad bad bad');
+
+let h = 5;
+let a = 0;
+while (a <4) {
+    var num = Number(prompt("guess a number"));
+    if (num == h) {
+        alert("ohh that is correct");
+        counter= counter+1;
+        console.log(num)
+        alert('you got'+counter+'point')
+        break;}
+    else if (1 < num && num <9 ) {
+        alert("too low");
+        console.log(num)
+    } else if (9< num || num<1) {
+        alert("too high");
+        console.log(num);
+    }
+    a = a + 1;
+    if (a==4)
+{ alert(" i wish you could hit the right answer, which is was 5");}
+}
+
+var b=0;
+while (b <7) {
+    var animal = prompt("guess what is the kind of animal i love? ");
+    animal=animal.toLowerCase()
+    if (animal == 'cat'|| animal== 'dog' ||animal=='bird' ) {
+        alert("ohh that is correct");
+        counter = counter+1;
+        console.log(animal);
+        break;
+    }
+    else  {
+        alert("that is wrong");
+
+    }
+    b = b+ 1;
+    if (b==7)
+{ alert(" i wish you know this info about me , i love cat ,dog and bird ");
+
+}
+}
+
+alert('your total point is '+ counter );
 
 
